947b05cfd04b04a75ea1864ece46981d22e37b29
walleeva2018 <walleeva2018@gmail.com>
2021-03-05 11:45:17 +0600

    ar na

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,7 @@ router.get('/',async(req,res)=>{
    let books
     try{
 
-        books=await Book.find().sort({createdAt: 'desc'}).limit(10).exec()
+        books=await Book.find().sort({createdAt: 'desc'}).exec()
    }catch{
         books=[]
    }
